11804af7690dbb873f453582594fc01bc73c6933
seattleninja <seattleninja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 10:53:45 +0000

    added screenshot-1.png added screenshot-2.png updated version
    git-svn-id: https://plugins.svn.wordpress.org/ninja-font-awesome-social-widget/trunk@1136071 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seattle-ninja-social.php
+++ seattle-ninja-social.php
@@ -3,7 +3,7 @@
    Plugin Name: Seattle Ninja Social
    Plugin URI: http://seattleninja.com/wordpress/plugins
    Description: Font-Awesome Based social icon widget that allows you to enter the icon by name and link. 
-   Version: 1.2
+   Version: 1.4
    Author: Ken Fujimoto
    Author URI: http://www.ninjajournal.com
    License: GPLv2 or later
